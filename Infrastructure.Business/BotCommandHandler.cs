769403dc3c3125090f19a5b8136fd14ff16aeb1a
Gandoler <gl.krutoi@mail.ru>
2025-03-16 15:16:48 +0300

    +логи в массаж хендлари

--- Infrastructure.Business/BotCommandHandler.cs
+++ Infrastructure.Business/BotCommandHandler.cs
@@ -20,12 +20,26 @@ public class BotCommandHandler: IBotCommandHandler
     
     public async Task HandleMessageAsync(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
     {
+        _logger.Information("Получено сообщение от {UserId}: {MessageText}", message.From?.Id, message.Text);
+    
         if (message.Text != null && message.Text.StartsWith("/start"))
         {
-            
-            await _tgSubProxy.SendTgSubVerification(new TgIdAndToken{Id=message.From.Id,Token=message.Text});
-            _logger.Information("Запрос на верификацию Telegram отправлен для {UserId}", message.From.Id);
-
+            _logger.Information("Обнаружена команда /start от {UserId}", message.From?.Id);
+        
+            if (message.From != null)
+            {
+                await _tgSubProxy.SendTgSubVerification(new TgIdAndToken
+                    { Id = message.From.Id, Token = message.Text });
+                _logger.Information("Запрос на верификацию Telegram отправлен для {UserId}", message.From.Id);
+            }
+            else
+            {
+                _logger.Warning("Сообщение с командой /start не содержит информации о пользователе.");
+            }
+        }
+        else
+        {
+            _logger.Debug("Сообщение не является командой /start.");
         }
     }
 }
