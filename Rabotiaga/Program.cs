3f80aeb2d90c7fd258a5f1a30bb53c8cb0364a0b
Gandoler <gl.krutoi@mail.ru>
2025-03-26 13:52:21 +0300

    обнова сборщика

--- Rabotiaga/Program.cs
+++ Rabotiaga/Program.cs
@@ -15,73 +15,84 @@ using Rabotiaga;
 using Serilog;
 using Telegram.Bot;
 
-var builder = Host.CreateDefaultBuilder(args)
-    .ConfigureAppConfiguration((hostContext, config) =>
-    {
-        // Используем настройки из окружения для подключения
-        var env = hostContext.HostingEnvironment;
-        if (hostContext.HostingEnvironment.IsDevelopment())
+// Инициализация Serilog до создания хоста
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/logs.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+try
+{
+    var builder = Host.CreateDefaultBuilder(args)
+        .UseSerilog()
+        .ConfigureAppConfiguration((hostContext, config) =>
         {
-            config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
-        }
-        else
+            var env = hostContext.HostingEnvironment;
+            if (env.IsDevelopment())
+            {
+                config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            }
+            else
+            {
+                config.AddEnvironmentVariables();
+            }
+        })
+        .ConfigureServices((hostContext, services) =>
         {
-            // В режиме продакшн или публикации - настройки загружаем из переменных окружения
-            config.AddEnvironmentVariables();
-        }
-    })
-    .ConfigureServices((hostContext, services) =>
-    {
-        var configuration = hostContext.Configuration;
+            var configuration = hostContext.Configuration;
 
-        // Чтение настроек из окружения или конфигурационного файла
-        var apiKey = configuration["TelegramBot:ApiKey"]
-                     ?? throw new Exception("ApiKey is missing");
-        var dbProxy = configuration["ApiSettings:DbProxy"]
-                      ?? throw new Exception("DbProxy is missing");
-        var tgSub = configuration["ApiSettings:TGSUBS"]
-                     ?? throw new Exception("TGSUBS is missing");
+            var apiKey = configuration["TelegramBot:ApiKey"]
+                         ?? throw new Exception("ApiKey is missing");
+            var dbProxy = configuration["ApiSettings:DbProxy"]
+                          ?? throw new Exception("DbProxy is missing");
+            var tgSub = configuration["ApiSettings:TGSUBS"]
+                         ?? throw new Exception("TGSUBS is missing");
 
-        // Регистрируем зависимости
-        services.AddSingleton(new TelegramBotClient(apiKey));
-        services.AddSingleton<IBotCommandHandler, BotCommandHandler>();
-        services.AddSingleton<ITgSubProxy, TgSubProxy>(); 
-        services.AddSingleton<ICongratulationService, CongratulationService>();
+            Log.Information("apiKey = {ApiKey}", apiKey);
+            Log.Information("dbProxy = {DbProxy}", dbProxy);
+            Log.Information("tgSub = {TgSub}", tgSub);
 
-        services.AddSingleton<IGetCongr, GetCongr>(); 
-        services.AddSingleton<IGetPozdrikId, GetPozdrikId>(); 
-        services.AddSingleton<IGetTgId, GetTgId>(); 
-        services.AddSingleton<IGetTodayBDayFriends, GetTodayBDayFriends>(); 
-        services.AddSingleton<ITgBotRepository, TgBotRepository>();
-        services.AddTransient<IGetTodayBDayFriends, GetTodayBDayFriends>();
+            services.AddSingleton(new TelegramBotClient(apiKey));
+            services.AddSingleton<IBotCommandHandler, BotCommandHandler>();
+            services.AddSingleton<ITgSubProxy, TgSubProxy>(); 
+            services.AddSingleton<ICongratulationService, CongratulationService>();
 
-        services.AddHttpClient("DbProxyClient", client =>
-        {
-            var dbProxyUrl = configuration["ApiSettings:DbProxy"] ?? throw new Exception("DbProxy is missing in configuration");
-            client.BaseAddress = new Uri(dbProxyUrl);
-        });
+            services.AddSingleton<IGetCongr, GetCongr>(); 
+            services.AddSingleton<IGetPozdrikId, GetPozdrikId>(); 
+            services.AddSingleton<IGetTgId, GetTgId>(); 
+            services.AddSingleton<IGetTodayBDayFriends, GetTodayBDayFriends>(); 
+            services.AddSingleton<ITgBotRepository, TgBotRepository>();
+            services.AddTransient<IGetTodayBDayFriends, GetTodayBDayFriends>();
 
-        services.AddHttpClient("TgSubClient", client =>
-        {
-            var tgSubUrl = configuration["ApiSettings:TGSUBS"] ?? throw new Exception("TGSUBS is missing in configuration");
-            client.BaseAddress = new Uri(tgSubUrl);
-        });
+            services.AddHttpClient("DbProxyClient", client =>
+            {
+                client.BaseAddress = new Uri(dbProxy);
+            });
 
-        // Добавляем недостающие зависимости
-        services.Configure<HostOptions>(options =>
-        {
-            options.BackgroundServiceExceptionBehavior = BackgroundServiceExceptionBehavior.Ignore;
-        });
+            services.AddHttpClient("TgSubClient", client =>
+            {
+                client.BaseAddress = new Uri(tgSub);
+            });
 
-        services.AddLogging();
-        services.AddSingleton<TelegramBotService>();
-        services.AddHostedService<Worker>();
-        Log.Information($"apiKey = {apiKey}");
-        Log.Information($"dbProxy = {dbProxy}");
-        Log.Information($"tgSub = {tgSub}");
-    })
-    .UseSerilog((context, config) => config.WriteTo.Console().WriteTo.File("logs/logs.txt"))
-    
-    .Build();
+            services.Configure<HostOptions>(options =>
+            {
+                options.BackgroundServiceExceptionBehavior = BackgroundServiceExceptionBehavior.Ignore;
+            });
 
-await builder.RunAsync();
+            services.AddLogging();
+            services.AddSingleton<TelegramBotService>();
+            services.AddHostedService<Worker>();
+        })
+        .Build();
+
+    await builder.RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Приложение завершилось с критической ошибкой");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
